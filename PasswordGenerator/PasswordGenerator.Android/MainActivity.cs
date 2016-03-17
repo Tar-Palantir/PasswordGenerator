f40a9924feca1cbab481b7aeefef24ff3f42eb9e
陈明 <astaldo@yeah.net>
2016-03-17 18:09:55 +0800

    修改部分WPF程序的界面布局 开始Android程序的界面搭建

--- PasswordGenerator/PasswordGenerator.Android/MainActivity.cs
+++ PasswordGenerator/PasswordGenerator.Android/MainActivity.cs
@@ -11,8 +11,6 @@ namespace PasswordGenerator.Android
     [Activity(Label = "PasswordGenerator.Android", MainLauncher = true, Icon = "@drawable/icon")]
     public class MainActivity : Activity
     {
-        int count = 1;
-
         protected override void OnCreate(Bundle bundle)
         {
             base.OnCreate(bundle);
@@ -20,11 +18,6 @@ namespace PasswordGenerator.Android
             // Set our view from the "main" layout resource
             SetContentView(Resource.Layout.Main);
 
-            // Get our button from the layout resource,
-            // and attach an event to it
-            Button button = FindViewById<Button>(Resource.Id.MyButton);
-
-            button.Click += delegate { button.Text = string.Format("{0} clicks!", count++); };
         }
     }
 }
